0077d19c0c0d7a24c5d640935d957f35a4c13f50
Sue Vang <vsue98@gmail.com>
2026-08-04 09:59:30 +0000

    Activity 2

--- vault.py
+++ vault.py
@@ -5,6 +5,14 @@ class AlienVault:
   def capture(self,alien_name):
     self.occupant = alien_name
     print(f'SYSTEM: {alien_name} has been secured in the vault.')
+  
+  def greet_alien(self):
+    if self.occupant is None:
+      return f'SYSTEM: The vault is empty.'
+    return f'{self.occupant} responds with strange look.'
+
 
   def __str__(self):
     return f'Vault occupant: {self.occupant}'
+
+  
